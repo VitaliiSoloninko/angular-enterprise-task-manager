2e60a97fe3acf900a0978c5d92a358415c08b093
22051988Di <vitalii.soloninko@gmail.com>
2024-08-29 15:25:33 +0200

    22. Property Binding & Outputting Computed Values - img [src]

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() + DUMMY_USERS.length);
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
