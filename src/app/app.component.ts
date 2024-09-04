4ea592c4dfdb0fe617d56f6b5a15c6c7e2d33309
22051988Di <vitalii.soloninko@gmail.com>
2024-09-04 15:10:23 +0200

    44. Step 1 - add tasks html +css. Step 2 - add  component task html + css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
@@ -8,7 +7,7 @@ import { UserComponent } from './user/user.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
