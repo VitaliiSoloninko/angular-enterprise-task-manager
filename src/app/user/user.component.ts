1bb690a87539b24596af4f354c6666525e173779
22051988Di <vitalii.soloninko@gmail.com>
2024-09-06 16:00:29 +0200

    56. ng-content (add css rounded around task, user) + new favicon + no scrolbar

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type User } from './user.model';
+import { CartComponent } from '../shared/cart/cart.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CartComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
